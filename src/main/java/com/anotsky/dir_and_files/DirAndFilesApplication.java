441fe17fc1e9c6654403f3f9edb94384fb48b14b
Alexadr Navolotsky <notsky12@gmail.com>
2023-10-05 18:57:10 +0300

    start

---
+++ src/main/java/com/anotsky/dir_and_files/DirAndFilesApplication.java
@@ -0,0 +1,13 @@
+package com.anotsky.dir_and_files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirAndFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirAndFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anotsky/dir_and_files/DirAndFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anotsky.dir_and_files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirAndFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
